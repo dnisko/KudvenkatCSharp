c6ccbd7e04a5f74783255f9df95ffd622f8c1cdc
dnisko <dnisko1@gmail.com>
2019-10-06 14:50:44 +0200

    LESSON 84

--- QueueAndStack84And85/Queue.aspx
+++ QueueAndStack84And85/Queue.aspx
@@ -8,8 +8,47 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
+        <<table style="font-family: Arial; border:1px solid black; text-align:center">
+            <tr>
+                <td><b> Counter 1</b></td>
+                <td></td>
+                <td><b> Counter 2</b></td>
+                <td></td>
+                <td><b> Counter 3</b></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="txtCounter1" runat="server" BackColor="#000099" Font-Size="Large" ForeColor="White" Width="150px"></asp:TextBox></td>
+                <td></td>
+                <td>
+                    <asp:TextBox ID="txtCounter2" runat="server" BackColor="#000099" Font-Size="Large" ForeColor="White"></asp:TextBox></td>
+                <td></td>
+                <td>
+                    <asp:TextBox ID="txtCounter3" runat="server" BackColor="#000099" Font-Size="Large" ForeColor="White"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnCounter1" runat="server" Text="Next" Width="150px" OnClick="btnCounter1_Click" /></td>
+                <td></td>
+                <td>
+                    <asp:Button ID="btnCounter2" runat="server" Text="Next" Width="150px" OnClick="btnCounter2_Click" /></td>
+                <td></td>
+                <td>
+                    <asp:Button ID="btnCounter3" runat="server" Text="Next" Width="150px" OnClick="btnCounter3_Click" /></td>
+            </tr>
+            <tr>
+                <td colspan="5"> <asp:TextBox ID="txtDisplay" runat="server" BackColor="#003300" Font-Size="Large" ForeColor="White" Width="500px"></asp:TextBox> </td>
+            </tr>
+            <tr>
+                <td colspan="5"> <asp:ListBox Font-Size="Large" ID="listTokens" runat="server" Width="300px"></asp:ListBox> </td>
+            </tr>
+            <tr>
+                <td colspan="5"> <asp:Button ID="btnPrintToken" runat="server" Text="Print Token" OnClick="btnPrintToken_Click" /> </td>
+            </tr>
+            <tr>
+                <td colspan="5"> <asp:Label ID="lblStatus" runat="server" Font-Size="Large"></asp:Label> </td>
+            </tr>
+        </table>
     </form>
 </body>
 </html>
--- QueueAndStack84And85/Queue.aspx.cs
+++ QueueAndStack84And85/Queue.aspx.cs
@@ -11,7 +11,68 @@ namespace QueueAndStack84And85
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["TokenQueue"] == null)
+            {
+                Queue<int> queueTokens = new Queue<int>();
+                Session["TokenQueue"] = queueTokens;
+            }
+        }
 
+        protected void btnPrintToken_Click(object sender, EventArgs e)
+        {
+            Queue<int> tokenQueue = (Queue<int>) Session["TokenQueue"];
+            lblStatus.Text = "There are " + tokenQueue.Count.ToString() + " customers before you in the queue";
+
+            if (Session["LastTokenNumberIssued"] == null)
+            {
+                Session["LastTokenNumberIssued"] = 0;
+            }
+
+            int nextTokenNumberToBeIssued = (int)Session["LastTokenNumberIssued"] + 1;
+            Session["LastTokenNumberIssued"] = nextTokenNumberToBeIssued;
+            tokenQueue.Enqueue(nextTokenNumberToBeIssued);
+
+            AddTokensToListBox(tokenQueue);
+        }
+
+        private void AddTokensToListBox(Queue<int> tokenQueue)
+        {
+            listTokens.Items.Clear();
+            foreach (int token in tokenQueue)
+            {
+                listTokens.Items.Add(token.ToString());
+            }
+        }
+
+        private void ServeNextCustomer(TextBox txtBox, int counterNumber)
+        {
+            Queue<int> tokenQueue = (Queue<int>)Session["TokenQueue"];
+            if (tokenQueue.Count == 0)
+            {
+                txtBox.Text = "No customers in the Queue";
+            }
+            else
+            {
+                int tokenNumberToBeServed = tokenQueue.Dequeue();
+                txtBox.Text = tokenNumberToBeServed.ToString();
+                txtDisplay.Text = "Token Number " + tokenNumberToBeServed + ", please go to Counter " + counterNumber;
+
+                AddTokensToListBox(tokenQueue);
+            }
+        }
+        protected void btnCounter1_Click(object sender, EventArgs e)
+        {
+            ServeNextCustomer(txtCounter1, 1);
+        }
+
+        protected void btnCounter2_Click(object sender, EventArgs e)
+        {
+            ServeNextCustomer(txtCounter2, 2);
+        }
+
+        protected void btnCounter3_Click(object sender, EventArgs e)
+        {
+            ServeNextCustomer(txtCounter3, 3);
         }
     }
 }
